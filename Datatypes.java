f2b3c30e8174b27d48020901e6d695fd6278fd34
Gnavya19 <navyaguddeti19@gmail.com>
2025-01-31 09:18:20 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,22 @@
+public class Datatypes {
+    public static void main(String[]args){
+        byte byteValue = 127;
+        System.out.print("Byte value: " + byteValue);
+        short shortValue = 32000;
+        System.out.print("Short value: " + shortValue);
+        int intValue = 500000;
+        System.out.print("Int value: " + intValue);
+        long longvalue = 19999980000L;
+        System.out.print("Long value: " + longvalue);
+        float floatValue = 9.8f;
+        System.out.print("Float value: " + floatValue);
+        double doubleValue = 3.1415926589793;
+        System.out.print("Double value: " + doubleValue);
+        char charValue = 'S';
+        System.out.print("Char value: " + charValue);
+        Boolean booleanValue = true;
+        System.out.print("Boolean value: " + booleanValue);
+        String stringValue = "Hello java!";
+        System.out.print("String value: " + stringValue);
+    }
+}
